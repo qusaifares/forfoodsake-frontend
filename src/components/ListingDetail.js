83ffe89d9cb196da90be0d1f570944ba1a7217de
Qusai Fares <qusaifares@gmail.com>
2020-09-20 17:29:07 -0400

    refactor: remove unused imports

--- src/components/ListingDetail.js
+++ src/components/ListingDetail.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { Typography, Box, Button, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Map from './Map';
